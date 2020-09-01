9611521b3f9e27700b21dcd703528b245f1dde21
Nataliesmyth <nataliesmyth11@gmail.com>
2020-09-01 14:25:03 -0400

    test

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,10 +1,18 @@
 # VARIABLE=VALUE sh curl-scripts/auth/sign-up.sh
 
 # don't use a password you use for any real websites!
-curl "API LINK GOES HERE" \
+
+# Remember: DATA is a JSON string
+curl "https://tic-tac-toe-api-production.herokuapp.com/sign-up" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-  --data ''
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
 
 echo
